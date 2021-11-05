e50a9ed346e4302425278ba89718fe79594bb67b
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-11-05 01:50:18 -0500

    [fix] change null operator

--- src/test/config.js
+++ src/test/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-  urlTarget: process.env.URL_TARGET ?? 'https://hiring.condorlabs.io/api/countries/all'
+  urlTarget: process.env.URL_TARGET || 'https://hiring.condorlabs.io/api/countries/all'
 }
